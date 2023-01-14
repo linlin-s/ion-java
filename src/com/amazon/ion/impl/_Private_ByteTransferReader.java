67bc77b6fb37cb1d1c6d21e07175e41ddcf233ef
Linlin Sun <linls@amazon.com>
2023-01-13 17:28:00 -0800

    Add PR content check to ion-test-driver workflow.

--- src/com/amazon/ion/impl/_Private_ByteTransferReader.java
+++ src/com/amazon/ion/impl/_Private_ByteTransferReader.java
@@ -17,7 +17,7 @@ package com.amazon.ion.impl;
 
 import com.amazon.ion.IonReader;
 import java.io.IOException;
-
+// Test only
 /**
  * An {@link IonReader} {@linkplain com.amazon.ion.facet facet} that can rapidly bulk-copy
  * Ion binary data under certain circumstances.
