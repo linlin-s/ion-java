aa4b3ed7eb03104d4cf750d249630dcedb1d763a
linlin-s <linlin-s@users.noreply.github.com>
2023-05-01 15:44:36 +0000

    Apply automatic changes

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var xValues = [1,2,3];
 var yValues = [4,5,6];
-var new_element = 1;
+var new_element = "${COMMIT_ID}"
 var new_xValues = xValues.push(new_element);
 var new_yValues = yValues.push(new_element);
 
