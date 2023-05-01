9170a959044e093eaaf2bb14584381f7b80f19cb
linlin-s <linlin-s@users.noreply.github.com>
2023-05-01 17:41:02 +0000

    Apply automatic changes

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var xValues = [1,2,3];
 var yValues = [4,5,6];
-var new_element = 912813e8b66412bba47ff5d2e0ac6afb234358d5
+var new_element = 95403c408095f5c5ffddb2d30d5cf559aebdbaf9
 var new_xValues = xValues.push(new_element);
 var new_yValues = yValues.push(new_element);
 
