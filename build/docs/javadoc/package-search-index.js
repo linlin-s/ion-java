532fe95be9ae5b58e7f29f22f58609ba8bce57dd
linlin-s <linlin-s@users.noreply.github.com>
2023-05-02 02:53:55 +0000

    Apply automatic changes

---
+++ build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.amazon.ion"},{"l":"com.amazon.ion.apps"},{"l":"com.amazon.ion.facet"},{"l":"com.amazon.ion.impl"},{"l":"com.amazon.ion.impl.bin"},{"l":"com.amazon.ion.impl.bin.utf8"},{"l":"com.amazon.ion.impl.lite"},{"l":"com.amazon.ion.system"},{"l":"com.amazon.ion.util"}]
