22170567eb88b31c208b982228e1e1b4eb17d15d
linlin-s <linls@amazon.com>
2023-05-02 01:08:20 -0700

    Create processSpeed.js

---
+++ processSpeed.js
@@ -0,0 +1,10 @@
+const originalSpeed = process.argv[2];
+const ion = require('ion-js');
+const fs = require('fs');
+const data = fs.readFileSync("./benchmark.ion")
+const ionValues = ion.load(data);
+const result = ionValues[0];
+const newSpeed = result.get("primaryMetric").get("score");
+const speed = JSON.parse(originalSpeed);
+speed.push(parseFloat(newSpeed.toString()));
+console.log(speed)
